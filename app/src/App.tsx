70c68448f2e5aa71efa08c868f9f7e91412a9805
ronttonen <ronttonen@pop-os.localdomain>
2021-07-16 16:43:12 +0300

    stuff

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import useSWR from "swr";
 import logo from "./logo.svg";
 
@@ -6,10 +6,30 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function App() {
   const { data } = useSWR<number[]>("/api/data", fetcher, {
-    refreshInterval: 1000,
+    refreshInterval: 500,
   });
 
   const d = data ?? [];
+
+  const [sendTasksEvery, setSendTasksEvery] = useState(0);
+
+  useEffect(() => {
+    let t: number;
+    const fn = async () => {
+      if (sendTasksEvery) {
+        await fetch("/api/data", { method: "POST" });
+        t = setTimeout(async () => {
+          await fn();
+        }, sendTasksEvery);
+      }
+    };
+    if (sendTasksEvery) {
+      fn();
+    }
+    return () => {
+      clearTimeout(t);
+    };
+  }, [sendTasksEvery]);
 
   return (
     <div>
@@ -20,6 +40,17 @@ function App() {
       >
         Send task
       </button>
+      <input
+        value={sendTasksEvery / 1000}
+        step="10"
+        placeholder="Send tasks every n second(s)"
+        type="number"
+        onChange={(e) => {
+          const val = e.target.value;
+          setSendTasksEvery(Math.floor(Number(val) * 1000));
+        }}
+      />
+      <button></button>
       <h1>Api results</h1>
       <div className="flex-col flex">
         {d.map((o) => (
